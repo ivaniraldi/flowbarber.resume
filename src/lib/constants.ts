bc360e26f523295adc6d0408a527837af71733c8
Ivan Iraldi (SykoTech) <iraldiban@gmail.com>
2025-11-27 13:30:59 +0000

    vamos a agregar platinado+corte a 150R$ en la lista de servicios, aument

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,9 +1,10 @@
 import type { PredefinedService } from './types';
 
 export const PREDEFINED_SERVICES: PredefinedService[] = [
-  { name: 'Corte padrão', price: 30 },
+  { name: 'Corte padrão', price: 35 },
   { name: 'Corte + barba', price: 50 },
   { name: 'Sobrancelha', price: 10 },
   { name: 'Corte navalhado', price: 35 },
   { name: 'Corte maquina', price: 20 },
+  { name: 'Platinado + corte', price: 150 },
 ];
