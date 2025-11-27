33ffa74996e55664a27fc0a3a3a8ffab612655c4
Ivan Iraldi (SykoTech) <iraldiban@gmail.com>
2025-11-27 13:38:40 +0000

    Estos son los precios de los servicios, actualizalos
    Corte padrão
    R$ 30

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,10 +1,11 @@
 import type { PredefinedService } from './types';
 
 export const PREDEFINED_SERVICES: PredefinedService[] = [
-  { name: 'Corte padrão', price: 35 },
-  { name: 'Corte + barba', price: 50 },
+  { name: 'Corte padrão', price: 30 },
+  { name: 'Corte + barba', price: 55 },
   { name: 'Sobrancelha', price: 10 },
-  { name: 'Corte navalhado', price: 35 },
+  { name: 'Corte navalhado', price: 40 },
   { name: 'Corte maquina', price: 20 },
   { name: 'Platinado + corte', price: 150 },
+  { name: 'Corte na 0', price: 35 },
 ];
